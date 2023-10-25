f2040d8e24e5f3c0ddd246b118aab893587d9cab
probot19 <prathamupadhye19@gmail.com>
2023-10-25 22:46:55 +0530

    1st Commit

---
+++ app/src/main/java/com/probot/miniprojecttry1/MainActivity.java
@@ -0,0 +1,62 @@
+package com.probot.miniprojecttry1;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import org.mariuszgromada.math.mxparser.*;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button speakButton;
+    private TextView txt,ans;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt=findViewById(R.id.result);
+        ans=findViewById(R.id.ans);
+        //speakButton = findViewById(R.id.speakButton);
+    }
+
+    public void getspeech(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txt.setText(result.get(0));
+                }
+                break;
+        }
+
+        String s = txt.getText().toString();
+        Expression exe = new Expression(s);
+        String res = String.valueOf(exe.calculate());
+        ans.setText(res);
+
+    }
+}
